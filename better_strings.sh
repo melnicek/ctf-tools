7dbf9c53f63fbc7a8bf88ce472b9141f367bf899
PETER MELNIčEK <41522529+melnicek@users.noreply.github.com>
2021-10-17 12:18:24 +0200

    Update better_strings.sh

--- better_strings.sh
+++ better_strings.sh
@@ -4,10 +4,19 @@ FILE=$1
 
 strings -n 10 $FILE
 
+echo
+echo "____ binwalk ____"
 binwalk $FILE
 
+echo
+echo "____ exiv2 ____"
 exiv2 $FILE
 
+echo
+echo "____ file ____"
 file $FILE
 
-echo 'Now use steghide/stegsolve for images'
+echo
+echo "____ todo ____"
+echo 'steghide/stegsolve for image files'
+echo 'sonic visualiser for audio files'
